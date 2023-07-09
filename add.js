f2ec3a59e36ee4c85438d4fcdd7cb5a024051452
hatatori <kcireh.i@gmail.com>
2023-07-09 16:53:28 -0300

    ok

--- add.js
+++ add.js
@@ -8,3 +8,15 @@ questions.all_disciplines_subjects().then(list=>{
     select.options(inp_discipline0, ["", ...Object.keys(list)])
     // console.log(Object.keys(list))
 })
+
+inp_discipline0.onchange=function(){
+    if(inp_discipline0.value != '')
+    {
+        inp_discipline.value = ""
+        inp_discipline.setAttribute('disabled', true)
+    }else{
+        inp_discipline.removeAttribute('disabled')
+    }
+
+    
+}
